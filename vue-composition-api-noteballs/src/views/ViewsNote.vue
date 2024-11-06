55f95513db2c34851567a3eaf75a6ab0f709216e
rodeanjos <148384733+rodeanjos@users.noreply.github.com>
2024-11-06 17:48:23 -0300

    Add notes design layout

--- vue-composition-api-noteballs/src/views/ViewsNote.vue
+++ vue-composition-api-noteballs/src/views/ViewsNote.vue
@@ -1,5 +1,31 @@
 <template>
-    <div class="notes">
-        <h1>Notes</h1>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-success">Submit</button>
+        </div>
+      </div>
     </div>
+    <div v-for="i in 3" :key="i" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          necessitatibus quis reiciendis incidunt omnis maiores veritatis
+          repudiandae ex non, doloremque assumenda id nesciunt architecto quae
+          aliquam eum hic debitis natus!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
